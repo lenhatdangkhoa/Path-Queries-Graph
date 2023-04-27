ac5ab0ea7550d07b8121494c1902c5484614890e
lenhatdangkhoa <kl58277@uga.edu>
2023-04-27 00:41:23 -0400

    updated Graph

--- p4/src/Graph.java
+++ p4/src/Graph.java
@@ -1,5 +1,51 @@
 package p4.src;
 
+class Vertex {
+
+    Vertex next;
+    String value;
+    Edge edge;
+
+    // Constructor
+    public Vertex() {
+        next = new Vertex();
+        value = "";
+        edge = new Edge();
+    } // vertex
+
+
+} // class
+
+
+/**
+ * Class Edge.
+ */
+class Edge {
+    String edgeValue;
+
+    public Edge() {
+        edgeValue = "";
+    } // edge()
+
+    public Edge(String value) {
+        edgeValue = value;
+    } // edge(val)
+} //
+
 public class Graph {
+    Vertex[] vertices;
+
+    public Graph() {
+        vertices = new Vertex[100];
+    } // graph()
+
+    public Graph(int size) {
+        vertices = new Vertex[size];
+    } // graph(size)
+
+
+    public void initializeGraph(String data) {
+
+    } // initializeGraph
 
 } // class
